261f5564ccfb181da8919721e365c7e1322e8660
amberkhatib2028 <ak1225@princeton.edu>
2026-09-21 02:38:01 +0200

    7/8 scripts done

--- Assets/Scripts/Amber's Scripts/DraggableIngredient.cs
+++ Assets/Scripts/Amber's Scripts/DraggableIngredient.cs
@@ -1,16 +1,73 @@
 using UnityEngine;
 
+// goes on each ingredient so player can drag it onto the plate
 public class DraggableIngredient : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // what type of piece is this
+    public IngredientType type;
+
+    // the plate we're dropping onto
+    public SandwichStack stack;
+
+    // how close to the plate the drop has to be to count
+    public float snapDistance = 1f;
+
+    // where the piece started
+    Vector3 startPosition;
+
+    // true while the player is holding this piece
+    bool dragging = false;
+
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    // called whe mouse is pressed on object's collider
+    void OnMouseDown()
     {
-        
+        dragging = true;
+    }
+
+    // called when mouse is held down on this object
+    void OnMouseDrag()
+    {
+        if (dragging)
+        {
+            transform.position = GetMouseWorldPosition();
+        }
+    }
+
+    // called when the player lets go
+    void OnMouseUp()
+    {
+        dragging = false;
+
+        // dropped too far from the plate
+        if (Vector3.Distance(transform.position, stack.transform.position) > snapDistance)
+        {
+            ReturnHome();
+            return;
+        }
+
+        // right place, but wrong ingredient for this step
+        if (!stack.TryPlace(this))
+        {
+            ReturnHome();
+        }
+    }
+
+    // snap back to where it started
+    void ReturnHome()
+    {
+        transform.position = startPosition;
+    }
+
+    // gets current position of mouse
+    Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f;
+        return mousePosition;
     }
 }
--- Assets/Scripts/Amber's Scripts/SandwichStack.cs
+++ Assets/Scripts/Amber's Scripts/SandwichStack.cs
@@ -2,15 +2,63 @@ using UnityEngine;
 
 public class SandwichStack : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // correct order to build in
+    public IngredientType[] recipe;
+
+    // where the next layer goes
+    public Transform nextLayer;
+
+    // vertical gap between each layer
+    public float layerHeight = 0.2f;
+
+    // guide card on the side
+    public RecipeGuide guide;
+
+    // how many layers are down so far/index of next ingredient
+    int placedCount = 0;
+
     void Start()
     {
-        
+        guide.Build(recipe);
     }
 
-    // Update is called once per frame
-    void Update()
+    // is the whole sandwich complete
+    public bool IsComplete()
     {
-        
+        return placedCount >= recipe.Length;
+    }
+
+    // try to add a piece to the stack; has to be in the correct order
+    public bool TryPlace(DraggableIngredient piece)
+    {
+        // sandwich is done
+        if (IsComplete())
+        {
+            return false;
+        }
+
+        // the only ingredient allowed right now
+        IngredientType expected = recipe[placedCount];
+
+        // wrong one
+        if (piece.type != expected)
+        {
+            return false;
+        }
+
+        // put the piece where the next layer goes
+        piece.transform.position = nextLayer.position;
+
+        // move up the nextLayer
+        Vector3 temp = nextLayer.position;
+        temp.y += layerHeight;
+        nextLayer.position = temp;
+
+        // tick it off on the guide card
+        guide.MarkDone(placedCount);
+
+        placedCount++;
+        return true;
     }
 }
