fcda7a223a71f8befafe5d85cc161213e9a706e5
sandovalkylie31-maker <sandovalkylie31@gmail.com>
2026-09-24 23:33:46 +0200

    almost final version of my minigame

---
+++ Assets/Scripts/Kylie Scripts/ClothingSpawner.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class ClothingSpawner : MonoBehaviour
+{
+
+    [Serializable] 
+    public class Clothing
+    {
+        public GameObject prefab;
+        public bool isFishingGear;
+    }
+
+    [SerializeField] private Clothing[] clothingItems;
+    [SerializeField] private Transform spawnPoint;
+
+    private GameObject currentItem;
+    private bool currentItemIsFishingGear;
+
+    public bool CurrentItemIsFishingGear => currentItemIsFishingGear;
+
+    int randomIndex;
+    public void SpawnRandomItem()
+    {
+        if(currentItem != null)
+        {
+            Destroy(currentItem);
+        }
+        {
+            
+            randomIndex = UnityEngine.Random.Range(0, clothingItems.Length);
+        }
+
+        Clothing selectedClothing = clothingItems[randomIndex];
+
+        currentItem = Instantiate(selectedClothing.prefab, spawnPoint.position, spawnPoint.rotation);
+
+        currentItemIsFishingGear = selectedClothing.isFishingGear; 
+
+    }
+
+    public void ClearItem()
+    {
+        if(currentItem != null)
+        {
+            Destroy(currentItem);
+            currentItem = null;
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Kylie Scripts/ShoppingGameManager.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SocialPlatforms.Impl;
+
+
+public class ShoppingGameManager : MonoBehaviour
+{
+    [SerializeField] private int totalQuestions = 10;
+
+    [SerializeField] private ClothingSpawner clothingSpawner;
+
+    private int currentQuestion;
+    private int playerScore;
+
+    private ShopControls playerControls;
+
+ 
+    private void Awake()
+    {
+        playerControls = new ShopControls();
+    }
+
+
+    private void OnEnable()
+    {
+        playerControls.ShoppingController.Enable();
+        playerControls.ShoppingController.Checkout.performed += OnCheckout;
+        playerControls.ShoppingController.Discard.performed += OnDiscard;
+
+    }
+    private void OnDisable()
+    {
+        playerControls.ShoppingController.Checkout.performed -= OnCheckout;
+        playerControls.ShoppingController.Discard.performed -= OnDiscard;
+
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartGame();
+    }
+
+    private void OnCheckout(InputAction.CallbackContext context)
+    {
+        SubmitAnswer(true);
+    }
+
+    private void OnDiscard(InputAction.CallbackContext context)
+    {
+        SubmitAnswer(false);
+    }
+   
+    public void StartGame()
+    {
+        currentQuestion = 0;
+        playerScore = 0;
+
+        NextItem();
+    }
+
+
+    private void NextItem()
+    {
+        if (currentQuestion >= totalQuestions)
+        {
+            EndGame();
+            return;
+        }
+
+        currentQuestion++;
+
+        clothingSpawner.SpawnRandomItem();
+    }
+
+
+    private void SubmitAnswer(bool FishingGear)
+    {
+        bool correct = FishingGear == clothingSpawner.CurrentItemIsFishingGear;
+
+        if (correct)
+        {
+            playerScore++;
+            Debug.Log("Correct!");
+        }
+        else
+        {
+            Debug.Log("Incorrect!");
+        }
+
+        Debug.Log("Score: " + playerScore +  "/" + currentQuestion);
+        NextItem();
+    }
+
+    private void EndGame()
+    {
+        clothingSpawner.ClearItem();
+
+    }
+
+
+
+
+}
